66a164338251578e638278fbf7c59868c7aaea3d
Tusk <101817510+Chimimin@users.noreply.github.com>
2022-03-17 18:59:32 +0300

    Add "Hello"

--- file.py
+++ file.py
@@ -5,3 +5,4 @@ while i !=  11 or s.isnumeric() != True:
     s = input("Yf xnj ns ;vfk&")
     i = len(s)
 print(s[0] +" ("+s[1]+s[2]+s[3]+") "+s[4]+s[5]+s[6]+"-"+s[7]+s[8]+"-"+s[9]+s[10])
+print("hello")
